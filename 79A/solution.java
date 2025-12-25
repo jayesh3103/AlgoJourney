d58e6cfa16d169c3332d419e9da17163926449f6
Jayesh Muley <115483595+jayesh3103@users.noreply.github.com>
2025-12-25 10:41:38 +0530

    Create solution.java

---
+++ 79A/solution.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        
+        while (true) {
+            if (x >= 2 && y >= 2) {
+                x -= 2;
+                y -= 2;
+            } else if (x >= 1 && y >= 12) {
+                x -= 1;
+                y -= 12;
+            } else if (y >= 22) {
+                y -= 22;
+            } else {
+                System.out.println("Hanako");
+                return;
+            }
+            
+            if (y >= 22) {
+                y -= 22;
+            } else if (x >= 1 && y >= 12) {
+                x -= 1;
+                y -= 12;
+            } else if (x >= 2 && y >= 2) {
+                x -= 2;
+                y -= 2;
+            } else {
+                System.out.println("Ciel");
+                return;
+            }
+        }
+    }
+}
