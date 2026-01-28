a1e34ca6780afe7552ca2534bde4b6b90928c948
Jayesh Muley <115483595+jayesh3103@users.noreply.github.com>
2026-01-28 07:01:38 +0530

    Implement solution to check for consecutive digits

---
+++ 96A/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        if (scanner.hasNext()) {
+            String s = scanner.next();
+            
+            if (s.contains("0000000") || s.contains("1111111")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        scanner.close();
+    }
+}
